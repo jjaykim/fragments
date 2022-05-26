0ce8667849aeccaf85352908c30cf494b272d613
JAY KIM <jaykim@JAYs-MacBook-Pro-2018.local>
2022-05-26 18:05:18 -0400

    WIP need to fix get.test.ts more

--- src/authorization/index.ts
+++ src/authorization/index.ts
@@ -1,16 +1,31 @@
 // Prefer Amazon Cognito
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
-	import('./cognito').then((res) => {
-		return [res.strategy, res.authenticate];
+	import('./cognito').then(({ strategy, authenticate }) => {
+		return { strategy, authenticate };
 	});
 }
 // Also allow for an .htpasswd file to be used, but not in production
-else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
-	import('./basic-auth').then((res) => {
-		return [res.strategy, res.authenticate];
+else if (process.env.HTPASSWD_FILE && process.env.NODE_ENV !== 'production') {
+	import('./basic-auth').then(({ strategy, authenticate }) => {
+		return { strategy, authenticate };
 	});
 }
 // In all other cases, we need to stop now and fix our config
 else {
 	throw new Error('missing env vars: no authorization configuration found');
 }
+
+// // src/authorization/index.js
+
+// // Prefer Amazon Cognito
+// if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+// 	module.exports = require('./cognito');
+// }
+// // Also allow for an .htpasswd file to be used, but not in production
+// else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
+// 	module.exports = require('./basic-auth');
+// }
+// // In all other cases, we need to stop now and fix our config
+// else {
+// 	throw new Error('missing env vars: no authorization configuration found');
+// }
